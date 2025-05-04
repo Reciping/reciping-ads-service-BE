61eb0c9556f0e19341ea420a2b8ad347c048f26b
beginninggrace <tldms123455@naver.com>
2025-05-05 02:16:03 +0900

    :tada: first commit

---
+++ src/main/java/com/three/recipingadsservicebe/RecipingAdsServiceBeApplication.java
@@ -0,0 +1,13 @@
+package com.three.recipingadsservicebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipingAdsServiceBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipingAdsServiceBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/three/recipingadsservicebe/RecipingAdsServiceBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.three.recipingadsservicebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipingAdsServiceBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
